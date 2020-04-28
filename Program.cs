72fde7ef7a4613f965222f1aedf875ef7dedd90c
opensam <opensam4@gmail.com>
2020-04-28 14:39:41 -0700

    Hello World

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chsarp_scratchpad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
